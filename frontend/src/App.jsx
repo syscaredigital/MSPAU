6605b6195105fc98fb04ff9eaa1e09ab31ff5ef6
syscare digital <syscaredigital@gmail.com>
2025-07-22 14:33:44 +0530

    roshan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-import HeaderHero from './components/HeaderHero.jsx';
+import Header from './components/header';
+import Hero from './components/hero';
 
 function App() {
   return (
-     <React.StrictMode>
-    <HeaderHero />
-  </React.StrictMode>
+    <div className="text-white relative overflow-x-hidden">
+      {/* Particle Background */}
+      <div id="particles-js" className="grid-pattern"></div>
+      
+      <Header />
+      <div className="h-20"></div> {/* Spacer for fixed header */}
+      <Hero />
+      
+      {/* Rest of your content would go here */}
+    </div>
   );
 }
 
