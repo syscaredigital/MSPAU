8be13679573903b6946f52a1d61f67c4d4234957
syscare digital <syscaredigital@gmail.com>
2025-07-22 17:48:23 +0530

    nabeela

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import Header from './components/header';
 import Hero from './components/hero';
+ import AboutUs from './components/aboutus.jsx';
 
 
 
 function App() {
   return (
-    <div className="text-white relative overflow-x-hidden">
-       {/* Particle Background */}
-       <div id="particles-js" className="grid-pattern"></div>
+    // <div className="text-white relative overflow-x-hidden">
+    //   {/* Particle Background */}
+    //   <div id="particles-js" className="grid-pattern"></div>
      
-      <Header />
-       <div className="h-20"></div> {/* Spacer for fixed header */}
-       <Hero />
+    //   <Header />
+    //   <div className="h-20"></div> {/* Spacer for fixed header */}
+    //   <Hero />
      
-       {/* Rest of your content would go here */}
+    //   {/* Rest of your content would go here */}
 
-     
+    //   <div className="relative">
+    //   <Sidebar />
+    //   <ThemeToggle />
+    //   <AboutUs />
+    // </div>
 
-    </div>
+    // </div>
 
-     
+     <div className="relative">
+      <AboutUs />
+     </div>
 
   );
 }
