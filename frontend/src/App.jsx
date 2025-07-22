7389df1683ea7ecd77a533d8521def16268b5982
syscaredigital <digital@syscare.com.au>
2025-07-22 14:52:33 +0530

    roshan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import HeaderHero from './components/HeaderHero.jsx';
+import AboutUs from './components/aboutus.jsx'
 
 function App() {
   return (
      <React.StrictMode>
-    <HeaderHero />
+    <AboutUs />
   </React.StrictMode>
   );
 }
