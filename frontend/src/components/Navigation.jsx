c678b79cb2bd3be2784a00c912a77c41ce262594
syscare digital <syscaredigital@gmail.com>
2025-08-20 09:33:50 +0530

    nabe

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -145,7 +145,7 @@ const Navigation = () => {
               <img
                 src="/logos/White-Sys.svg"
                 alt="SysCare Logo"
-                className="hidden md:block h-14 w-auto object-contain"
+                className="hidden md:block h-20 w-auto object-contain"
               />
               {/* Mobile Logo */}
               <img
